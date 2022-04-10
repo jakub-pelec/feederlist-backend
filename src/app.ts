a4f5fcece82d479255810c6de6ba153cc5c07763
jakub-pelec <kuba1pelec@gmail.com>
2022-04-10 22:50:21 +0200

    ENABLE CORS FOR LOCALHOST

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,17 @@ import express from "express";
 import dotenv from 'dotenv';
 import { ROUTES } from './constants/routes';
 import handlers from './handlers';
+import cors from 'cors';
 
 dotenv.config()
+const corsConfig = {
+    origin: 'localhost:3000',
+    optionsSuccessStatus: 200
+  }
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(cors(corsConfig));
 
 app.get(ROUTES.test, handlers.test);
 app.get(ROUTES.version, handlers.version);
