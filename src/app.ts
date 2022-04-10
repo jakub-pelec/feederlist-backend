93654ca03aa266681e6bdc8ef1ab73315eac11a8
jakub-pelec <kuba1pelec@gmail.com>
2022-04-10 22:15:43 +0200

    TEST ROUTE

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('siema');
+import express from "express";
+import { ROUTES } from './constants/routes';
+import handlers from './handlers';
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get(ROUTES.test, handlers.test)
+
+app.listen(PORT, () => {
+	console.log(`Listening on PORT ${PORT}`);
+});
---
+++ src/handlers/test.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from "express";
+
+export default (req: Request, res: Response) => {
+	return res.status(200).send("Hello world!");
+};
